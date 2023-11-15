0dd17026f7cbb4a0d08f17d52e9f82176f84209d
Danny B <dbonnett1147@gmail.com>
2023-11-15 12:29:03 -0500

    note component started

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <BasicNote/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BasicNote from './components/BasicNote.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BasicNote
   }
 }
 </script>
---
+++ src/components/BasicNote.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="basic-note">
+    <h2>Here's a nice little icon</h2>
+    <img src="./../../public/1024824.png" alt="img not found">
+    <textarea name="note" id="note" cols="30" rows="10" v-model.lazy="note"></textarea>
+    <h2>What you just typed:</h2>
+    <p>{{ note }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "basic-note",
+    data() {
+      return {
+        note: ""
+      }
+    }
+  }
+</script>
+
+<style>
+  h2 {
+    color: green;
+  }
+</style>
